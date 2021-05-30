aca9c703f9bb327da50d08dee759e6d9b336262f
Elnathan John L. Salavarria <ejsalavarria84@gmail.com>
2021-05-30 21:15:34 +0800

    Created a parameter that checks the search result output depending on the product search. Added previous price and current price

--- my_amazon_scraper.py
+++ my_amazon_scraper.py
@@ -11,24 +11,41 @@ def amazon_search(query):
     driver.get("https://www.amazon.com/")
     search_box = driver.find_element_by_id('twotabsearchtextbox')
     search_box.send_keys(query)
-    # search_box.send_keys('drone')
     search_box.send_keys(Keys.ENTER)
     sleep(5)
-
+    dup_check = ""
     result = driver.find_elements_by_xpath('//*[@class="sg-col-inner"]')
     # print(result)
-    for item in result:
+    for item in result[3:]:
         try:
-            title = item.find_element_by_xpath('.//span[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
-            # price = item.find_element_by_xpath('.//span[@class="c13VH6"]').get_attribute('innerText')
+            try:
+                title = item.find_element_by_xpath('.//span[contains(@class, "a-size-medium a-color-base a-text-normal")]').get_attribute('innerText')
+            except:
+                title = item.find_element_by_xpath('.//span[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
+
+            # print(title)
+            if title == dup_check:
+                print('')
+            else:
+                print(title)
+                try:
+                    o_price = item.find_element_by_xpath('.//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]').get_attribute('innerText')
+                    print(o_price)
+                except:
+                    print('No discount')
+                try:
+                    c_price = item.find_element_by_xpath('.//span[@class="a-offscreen"]').get_attribute('innerText')
+                    print(c_price)
+                except:
+                    print('Product on exclusive pricing')
+
+
             # location = item.find_element_by_xpath('.//span[@class="c2i43- "]').get_attribute('innerText')
             #link = item.find_element_by_xpath('.//*[@class="c16H9d"]//a[contains(@age, "0")]').get_attribute('innerHTML')
             # title = item.find_element_by_xpath('.//*[@class="s-main-slot s-result-list s-search-results sg-row"]//data-asin//class//span//span//class//[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
-            print(title)
-            # print(price)
             # print(location)
-            #print(link)
+            # print(link)
             print('')
-
+            dup_check = title
         except:
-            print('error')
+            print('')
--- test.py
+++ test.py
@@ -11,37 +11,39 @@ search_box = driver.find_element_by_id('twotabsearchtextbox')
 search_box.send_keys('iphone11')
 search_box.send_keys(Keys.ENTER)
 sleep(5)
-
+dup_check = ""
 result = driver.find_elements_by_xpath('//*[@class="sg-col-inner"]')
-print(result)
-for item in result:
+# print(result)
+for item in result[3:]:
     try:
         try:
-            title = item.find_element_by_xpath('.//span[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
-            print(title)
-
-        except:
             title = item.find_element_by_xpath('.//span[contains(@class, "a-size-medium a-color-base a-text-normal")]').get_attribute('innerText')
-            print(title)
-        try:
-            o_price = item.find_element_by_xpath('.//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]').get_attribute('innerText')
-            print(o_price)
         except:
-            print('No discount')
+            title = item.find_element_by_xpath('.//span[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
 
-        try:
-            c_price = item.find_element_by_xpath('.//span[@class="a-offscreen"]').get_attribute('innerText')
-            print(c_price)
-        except:
-            print('Product on exclusive pricing')
+        # print(title)
+        if title == dup_check:
+            print('')
+        else:
+            print(title)
+            try:
+                o_price = item.find_element_by_xpath('.//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]').get_attribute('innerText')
+                print(o_price)
+            except:
+                print('No discount')
+            try:
+                c_price = item.find_element_by_xpath('.//span[@class="a-offscreen"]').get_attribute('innerText')
+                print(c_price)
+            except:
+                print('Product on exclusive pricing')
+
 
         # location = item.find_element_by_xpath('.//span[@class="c2i43- "]').get_attribute('innerText')
         #link = item.find_element_by_xpath('.//*[@class="c16H9d"]//a[contains(@age, "0")]').get_attribute('innerHTML')
         # title = item.find_element_by_xpath('.//*[@class="s-main-slot s-result-list s-search-results sg-row"]//data-asin//class//span//span//class//[contains(@class, "a-size-base-plus a-color-base a-text-normal")]').get_attribute('innerText')
-
-
         # print(location)
-        #print(link)
+        # print(link)
         print('')
+        dup_check = title
     except:
         print('error')
