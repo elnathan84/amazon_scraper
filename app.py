820e8d67fe7c0303704626a00b9ef1e9833fed50
Elnathan John L. Salavarria <ejsalavarria84@gmail.com>
2021-06-01 20:06:30 +0800

    Added loading bar

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ import pandas as pd
 import time
 import base64
 
-#Webapp
-st.set_page_config(page_title = "Amazon Webscraper")
-
-desc = "Webscrape Amazon through Product URL or Amazon Search\
-        You can either enter the Product URL or just search Amazon through here.\
-        This app was developed with Streamlit and Selenium for the webapp and scraping features\
-        Check out our github repository! (https://github.com/gnsslrrcs/dlsud-cpse325-2021/tree/major-exam/scraping/Amazon).\
-        "
-
-st.title("Amazon Webscraper")
-st.subheader("By Jopak's Angels")
-st.markdown(desc)
-st.subheader("Select the option")
-select_input = st.radio("Select Input:", ["Product URL", "Search Amazon"])
-
-
 def load_searchdata():
     progress_bar = st.progress(0)
     status_text = st.empty()
@@ -37,15 +21,17 @@ def load_searchdata():
         time.sleep(0.01)
 
         while i == 50:
-            amazon_search(text)
+            with st.spinner("The angels are scraping..."):
+                amazon_search(text)
+            st.success('We have returned with glorious data!')
             df = pd.DataFrame.from_dict(search_dict)
             # df = df.drop(df.index[7])
             # dfr = df.reset_index(drop=True)
             progress_bar.progress(i + 1)
             i += 1
+    status_text.empty()
     st.balloons()
     st.dataframe(df)
-    status_text.text('Done!')
 
     csv = df.to_csv(index=True)
     b64 = base64.b64encode(csv.encode()).decode()
@@ -54,20 +40,36 @@ def load_searchdata():
     st.markdown(href, unsafe_allow_html=True)
 
 
+# Webapp
+st.set_page_config(page_title = "Amazon Webscraper")
+
+desc = "Webscrape Amazon through Product URL or Amazon Search.\
+        You can either enter the Product URL or just search Amazon through here.\
+        This app was developed with Streamlit and Selenium for the webapp and scraping features.\
+        Check out our github repository! (https://github.com/gnsslrrcs/dlsud-cpse325-2021/tree/major-exam/scraping/Amazon).\
+        "
+st.image('./logo.png', width=300)
+st.title("Amazon Webscraper")
+st.header("By Jopak's Angels")
+st.markdown(desc)
+st.subheader("Scraping Menu")
+select_input = st.radio("Select Input:", ["Product URL", "Search Amazon", "Amazon Top Sellers"])
+
+
+
 if select_input == "Product URL":
     url = st.text_input("URL")
     if st.button("Scrape It!"):
         print("")
-        # text = get_page_text(url)
-        # generate_output(text)
+        # placeholder(text)
+elif select_input == "Amazon Top Sellers":
+    st.button('Show Me!')
 
 else:
     text = st.text_area("Search Amazon")
     if st.button("Scrape It!"):
-        # print("")
         load_searchdata()
-
-        # generate_output(text)
+        search_dict.clear()
 
 
 
